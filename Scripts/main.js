4748e9f7fd315ad304117aea47c81b4f42c4c28e
Alex Noles <alexnoles@outlook.com>
2021-06-25 23:29:55 +0100

    website is even more based

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,8 @@
-
+var bool = true;
 function randTanuki(){
     const tanukiImage = document.getElementById('1');
-    tanukiImage.src = '/Images/tanuki2.jpg';
+    if(bool){tanukiImage.src = '/Images/tanuki2.jpg'; bool = false;}
+    else{tanukiImage.src = '/Images/tanuki.jpg'; bool = true;}
 }   
 
 $("#1").on("click", function(){
