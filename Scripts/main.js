a791e79056cbae484a31c46504ca7b83f4a19739
Alex Noles <alexnoles@outlook.com>
2021-06-25 23:22:53 +0100

    site is maybe based

--- Scripts/main.js
+++ Scripts/main.js
@@ -3,3 +3,7 @@ function randTanuki(){
     const tanukiImage = document.getElementById('1');
     tanukiImage.src = '/Images/tanuki2.jpg';
 }   
+
+$("#1").on("click", function(){
+  randTanuki();
+});
