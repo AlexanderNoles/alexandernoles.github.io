30eff517fd32916862cd8dee69971ec400ddff87
Alex Noles <alexnoles@outlook.com>
2021-06-25 23:06:29 +0100

    tanuki button

---
+++ Scripts/main.js
@@ -0,0 +1,9 @@
+let body = null;
+window.onload = function get_body() {
+
+    body = document.getElementsByTagName('body')[0];
+}
+
+function randTankui(){
+        body.style.backgroundImage = "url('/Images/tanuki2.jpg')";
+}   
