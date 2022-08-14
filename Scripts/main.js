2d8256116846cbd1e2467f8d499121c06b438406
Piturnah <peterhebden6@gmail.com>
2022-08-14 21:49:17 +0100

    da third tanuki
    it the tankui

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,8 +1,20 @@
-var bool = true;
+var bool = true; // var bool is staying
+let tanuki_index = 0;
 function randTanuki(){
     const tanukiImage = document.getElementById('1');
-    if(bool){tanukiImage.src = '/Images/tanuki2.jpg'; bool = false;}
-    else{tanukiImage.src = '/Images/tanuki.jpg'; bool = true;}
+    tanuki_index += 1;
+    tanuki_index = tanuki_index % 3;
+    switch (tanuki_index) {
+        case 0:
+	    tanukiImage.src = '/Images/tanuki.jpg'; 
+            break;
+        case 1:
+	    tanukiImage.src = '/Images/tanuki2.jpg'; 
+            break;
+        case 2:
+	    tanukiImage.src = '/Images/tanuki3.jpg'; 
+            break;
+    }
 }   
 
 $("#1").on("click", function(){
