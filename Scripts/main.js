294baf19cc3bbe3540e531b3ff2af97d183aebe6
Piturnah <peterhebden6@gmail.com>
2023-06-06 15:49:40 +0100

    Merge conflict? IUdk l;ost track of what git was doing

--- Scripts/main.js
+++ Scripts/main.js
@@ -6,26 +6,15 @@ if (stored_count !== null) {
 	count = parseInt(stored_count)
 }
 	 document.getElementById("2").innerHTML = count
+
 function randTanuki(){
     const tanukiImage = document.getElementById('1');
-    tanuki_index += 1;
-    tanuki_index = tanuki_index % 3;
-    switch (tanuki_index) {
-        case 0:
-	    tanukiImage.src = '/Images/tanuki.jpg'; 
-            break;
-        case 1:
-	    tanukiImage.src = '/Images/tanuki2.jpg'; 
-            break;
-        case 2:
-	    tanukiImage.src = '/Images/tanuki3.jpg'; 
-            break;
-    }
+    bool = bool? false : bool == false ? null : true; 
+    tanukiImage.src = bool? 'Images/tanuki.jpg' : bool == false ? 'Images/tanuki2.jpg' : 'Images/tanuki3.jpg'; 
 	count += 1;
 	localStorage.setItem("meme", count);
 	document.getElementById("2").innerHTML = count
-}   
-
+}
 $("#1").on("click", function(){
   randTanuki();
 });
